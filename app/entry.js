d738c47db6d20a8f1e742945c8a145b25bd0544c
haru221 <haru221@mocha.ocn.ne.jp>
2022-01-30 22:30:47 +0900

    cryptoモジュールの追加

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,8 @@
 'use strict';
 import dc from 'damage-calc';
+import crypto from 'crypto';
 const root = document.getElementById('root');
-root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
-  + dc.effectiveDamage(100, 50, 30) + '</p>';
+const cryptoString = crypto.randomBytes(8).toString('hex');
+const damageString = '攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
+  dc.effectiveDamage(100, 50, 30);
+root.innerHTML = `<p>${damageString}</p><p>${cryptoString}</p>`;
